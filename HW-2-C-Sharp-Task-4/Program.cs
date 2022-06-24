4ed6d13c580add012cf11b3c9d102828f3fba495
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2022-06-24 14:59:14 +0300

    correct code

--- HW-2-C-Sharp-Task-4/Program.cs
+++ HW-2-C-Sharp-Task-4/Program.cs
@@ -12,37 +12,35 @@ namespace HW_2_C_Sharp_Task_4
         static void Main(string[] args)
         {
             //Добавление матрицы
-            double[,] CreateArray()
+            int[,] CreateArray()
             {
                 Console.WriteLine("Input values of rows and columns for creating a matrix:");
                 Console.Write("Input a number of matrix`s rows - ");
                 int arrX = int.Parse(Console.ReadLine());
                 Console.Write("Input a number of matrix`s columns - ");
                 int arrY = int.Parse(Console.ReadLine());
-                double [,] array = new double[arrX+1, arrY+1];
-                array[0, 0] = arrX;
-                array[0, 1] = arrY;
+                int [,] array = new int[arrX, arrY];
                 return array;
             }
             //Заполнение ячеек матрицы значениями
-            void FillArray (double [,] array)
+            void FillArray (int [,] array)
             {
                 Console.WriteLine("Enter numbers into cells of the matrix");
-                for (int i = 1; i <= Convert.ToInt16(array[0, 0]); i++)
+                for (int i = 0; i < array.GetLength(0); i++)
                 {
-                    for (int j = 1; j <= Convert.ToInt16(array[0, 1]); j++)
+                    for (int j = 0; j < array.GetLength(1); j++)
                     {
                         Console.Write($"[{i}, {j}] - ");
-                        array[i, j] = double.Parse(Console.ReadLine());
+                        array[i, j] = int.Parse(Console.ReadLine());
                     }
                 }
             }
             //Показ значений ячеек матрицы
-            void ShowArray(double[,] array)
+            void ShowArray(int[,] array)
             {
-                for (int i = 1; i <= Convert.ToInt16(array[0, 0]); i++)
+                for (int i = 0; i < array.GetLength(0); i++)
                 {
-                    for (int j = 1; j <= Convert.ToInt16(array[0, 1]); j++)
+                    for (int j = 0; j < array.GetLength(1); j++)
                     {
                         Console.Write($"{array[i, j]}  ");
 
@@ -51,26 +49,27 @@ namespace HW_2_C_Sharp_Task_4
                 }
             }
 
-            double[,] matrix, matrix1, matrix2;
-            Console.WriteLine("Choose a number that you want to do with the matrix:\n1. Multiply by number;\n2. Add two matrices;\n3. Multiply two matrices.\n") ;
+            int[,] matrix, matrix1, matrix2;
+            Console.WriteLine("Input a number of position that you want to do with the matrix:\n1. Multiply by number;\n2. Add two matrices;\n3. Multiply two matrices.\n") ;
             char key = Convert.ToChar(Console.ReadKey().Key);
             if (key == '1')
             {
                 Console.Clear();
                 Console.Write("Enter a number by which the matrix will be multiplied - ");
-                double num = double.Parse(Console.ReadLine());
+                int num = int.Parse(Console.ReadLine());
                 matrix1 = CreateArray();
-                matrix2 = (double[,])matrix1.Clone();
+                matrix2 = (int[,])matrix1.Clone();
                 Console.Write("Enter a number by which the matrix will be multiplied - ");
                 FillArray(matrix1);
-                for (int i = 1; i <= Convert.ToInt16(matrix2[0, 0]); i++)
+                for (int i = 0; i < matrix2.GetLength(0); i++)
                 {
-                    for (int j = 1; j <= Convert.ToInt16(matrix2[1, 0]); j++)
+                    for (int j = 0; j < matrix2.GetLength(1); j++)
                         matrix2[i, j] = matrix1[i, j] * num;
                 }
+                Console.Clear();
                 Console.WriteLine("Your matrix:");
                 ShowArray(matrix1);
-                Console.WriteLine("New matrix:");
+                Console.WriteLine($"New matrix after multiply by number {num}:");
                 ShowArray(matrix2);
             }
             else if (key == '2')
@@ -78,17 +77,18 @@ namespace HW_2_C_Sharp_Task_4
                 Console.Clear();
                 Console.WriteLine("Input values of rows and columns for create and addition of two matrices:");
                 matrix1 = CreateArray();
-                matrix2 = (double[,])matrix1.Clone();
-                matrix = (double[,])matrix1.Clone();
+                matrix2 = (int[,])matrix1.Clone();
+                matrix = (int[,])matrix1.Clone();
                 Console.WriteLine("Input data into first matrix:");
                 FillArray(matrix1);
                 Console.WriteLine("Input data into second matrix:");
                 FillArray(matrix2);
-                for (int i = 1; i <= Convert.ToInt16(matrix1[0, 0]); i++)
+                for (int i = 0; i < matrix1.GetLength(0); i++)
                 {
-                    for (int j = 1; j <= Convert.ToInt16(matrix1[0, 1]); j++)
+                    for (int j = 0; j < matrix1.GetLength(1); j++)
                         matrix[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
+                Console.Clear();
                 Console.WriteLine("Your first matrix:");
                 ShowArray(matrix1);
                 Console.WriteLine("Your second matrix:");
@@ -100,21 +100,25 @@ namespace HW_2_C_Sharp_Task_4
             {
                 Console.Clear();
                 Console.WriteLine("Input values of rows and columns for create and multiply of two matrices:");
-                Console.WriteLine("Rule: number of columns first matrix must be equal number of rows second matrix");
+                Console.WriteLine("Rule: amount of columns first matrix must be equal amount of rows second matrix\n");
                 matrix1 = CreateArray();
                 matrix2 = CreateArray();
                 if (matrix1[0, 1] == matrix2[0, 0])
                 {
-                    matrix = new double [Convert.ToInt16(matrix1[0, 0]+1), Convert.ToInt16(matrix2[0, 1]+1)];
+                    matrix = new int [matrix1.GetLength(0), matrix2.GetLength(1)];
                     Console.WriteLine("Input data in the first matrix:");
                     FillArray(matrix1);
                     Console.WriteLine("Input data in the second matrix:");
                     FillArray(matrix2);
-                    for (int i = 1; i <= Convert.ToInt16(matrix1[0, 0]); i++)
+                    for (int i = 0; i < matrix1.GetLength(0); i++)
                     {
-                        for (int j = 1; j <= Convert.ToInt16(matrix2[0, 1]); j++)
-                            matrix[i, j] = matrix1[i, j] * matrix2[j, i];
+                        for (int j = 0; j < matrix2.GetLength(1); j++)
+                        {
+                            for (int k = 0; k < matrix1.GetLength(1) ; k++)
+                                matrix[i, j] += matrix1[i, k] * matrix2[k, j];
+                        }
                     }
+                    Console.Clear();
                     Console.WriteLine("Your first matrix:");
                     ShowArray(matrix1);
                     Console.WriteLine("Your second matrix:");
